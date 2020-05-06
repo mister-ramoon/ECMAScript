ef6a0bfe6db60f82f0b9e07886be70d34c20a0aa
ramon <ramonruizrc@gmail.com>
2020-05-06 13:00:03 -0500

    Cambios de ECMAScript 7

---
+++ src/es7/index.js
@@ -0,0 +1,17 @@
+//Includes
+let numbers = [1, 2, 3, 7, 8];
+
+if(numbers.includes(7)){
+    console.log('Si se encuentra el valor 7');
+}
+else {
+    console.log('No se encuentra el valor 7');
+}
+
+//Potenciacion
+let base = 4;
+let exponent = 4;
+let result = base ** exponent;
+console.log(result);
+
+
